04e1809ed52f3be7ced5176cd5aac82aa5e87aa6
Nikita Pramod Nimbalkar <86878164+nikitanimbalkar@users.noreply.github.com>
2021-07-03 17:35:35 +0530

    HelloWorld

---
+++ HelloWorld2.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+
+public class HelloWorld2
+{
+public static void main(String[] args)
+{Scanner reader=new
+scanner(System.in);
+System.out.println("enter a number:");
+int number=reader.nextInt();
+System.out.println("You entered:"+number);
+}
+}
