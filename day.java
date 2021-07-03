00557218f41a8ad516aa2daeddf115a71261e15e
Nikita Pramod Nimbalkar <86878164+nikitanimbalkar@users.noreply.github.com>
2021-07-03 17:41:12 +0530

    Day

---
+++ day.java
@@ -0,0 +1,17 @@
+public class Day
+{
+public static void main(String args[])
+{
+int time=20;
+if(time<18)
+{
+System.out.println("Good day");
+}
+else if(time<16){
+System.out.println("Good evening");
+}
+else
+{
+System.out.println("Good night");
+}
+}}
